3b58ae614e718e1378cc05f5acce9057494531e3
Abdulrahamn Fahmy <75482475+abdulrhman500@users.noreply.github.com>
2022-10-14 22:14:24 +0200

    Removing Django Security key

--- Tasks/Tasks/settings.py
+++ Tasks/Tasks/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s5ohq$9#x!h7i-&h-!jqeoe(6g#)2qf(ox$0w&1!c2qnk_m5xy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
